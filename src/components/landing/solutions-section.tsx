dfb6f75c3adea96456cc3dbd20a0aa4efe95fc94
Cong Nguyen <congnguyen.vesper007@gmail.com>
2025-09-08 05:45:37 +0000

    Please update the image here to the newly chosen ones (_for element img_

--- src/components/landing/solutions-section.tsx
+++ src/components/landing/solutions-section.tsx
@@ -57,7 +57,7 @@ export function SolutionsSection() {
           </div>
           <div className="flex items-center justify-center">
             <Image
-              src="https://images.unsplash.com/photo-1674027444485-cec3da58eef4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwyfHxBSXxlbnwwfHx8fDE3NTcyNTQzODF8MA&ixlib=rb-4.1.0&q=80&w=1080"
+              src="https://images.unsplash.com/photo-1697577418970-95d99b5a55cf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxBcnRpZmljaWFsJTIwaW50ZWxsaWdlbmNlfGVufDB8fHx8MTc1NzMxMDI2OXww&ixlib=rb-4.1.0&q=80&w=1080"
               width="600"
               height="500"
               alt="Marketing Solution Diagram"
