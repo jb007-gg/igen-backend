02f5c86f5889c9f96627b6090b731f79686a2477
Cong Nguyen <congnguyen.vesper007@gmail.com>
2025-09-30 08:27:35 +0000

    can you make a copy about the section Ra đời từ Thực chiến, Dành cho Chủ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
         <ServicesSection />
         <SolutionsSection />
         <CoursesSection />
+        <AboutSection />
         <VisionSection />
         <TestimonialsSection />
         <ContactSection />
